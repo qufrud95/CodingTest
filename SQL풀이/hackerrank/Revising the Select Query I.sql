772e31f765a40844f82986507993151eb1a7f446
qufrud95 <69886466+qufrud95@users.noreply.github.com>
2021-08-11 16:19:50 +0900

    MODIFY
    https://www.hackerrank.com/challenges/revising-the-select-query/problem

--- SQL풀이/hackerrank/Revising the Select Query I.sql
+++ SQL풀이/hackerrank/Revising the Select Query I.sql
@@ -4,7 +4,6 @@ Query all columns for all American cities in the CITY table with populations lar
 
 The CITY table is described as follows:
 
-https://www.hackerrank.com/challenges/revising-the-select-query/problem
 
 
 SELECT *
